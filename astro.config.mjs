b7375f600e4175468a6d6d2a56d2e71ef9603707
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2025-09-28 19:03:12 +0100

    add linebreaks
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.rdhar.dev",
   integrations: [mdx(), sitemap()],
