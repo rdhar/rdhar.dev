b832d5e258d92911399c5fc7d2dfd3f814bf09a2
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2025-09-28 18:46:56 +0100

    deps: update tailwindcss
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-// import relativeLinks from "astro-relative-links";
+import tailwindcss from "@tailwindcss/vite";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.rdhar.dev",
-  integrations: [mdx(), sitemap(), tailwind()]
-  // relativeLinks(),
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
