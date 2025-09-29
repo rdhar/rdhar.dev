52bd4c71e75af7140a117d5bdf62c4dc5195bdcf
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2025-09-29 13:19:22 +0100

    deps: downgrade tailwindcss until proper upgrade path (#22)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://www.rdhar.dev",
-  integrations: [mdx(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [mdx(), sitemap(), tailwind()],
 });
