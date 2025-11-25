b5bdb02d39572cd98b9b9f1d4bc09f6ed906572c
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2025-11-25 22:12:55 +0000

    deps:: bump tailwindcss (#44)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  integrations: [mdx(), sitemap()],
   site: "https://www.rdhar.dev",
-  integrations: [mdx(), sitemap(), tailwind()],
+  vite: { plugins: [tailwindcss()] },
 });
