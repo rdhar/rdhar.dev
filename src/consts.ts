31a318812f1672edd4e0e20bd5b7bd61cdc8a56b
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2025-11-25 22:29:13 +0000

    content: constrain links (#48)

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Rishav Dhar",
-  EMAIL: "hi@rdhar.dev",
+  EMAIL: "hi",
   NUM_POSTS_ON_HOMEPAGE: 2,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 1,
